c8f14263f907547cd67a575aeec0f74932b6c425
Gabriel de Campos <gabodecampos@gmail.com>
2023-01-16 11:17:40 -0400

    home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Navbar from "../../components/navbar/index.jsx";
+
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+import Dashboard from "../../components/dashboard";
 
 export default function HomePage() {
+  // const user = false
+  //
+  // if (!user) {
+  //   return redirect("/");
+  // }
   return (
     <>
       <Navbar/>
+      <Dashboard/>
+      <Footer/>
     </>
   );
 }
