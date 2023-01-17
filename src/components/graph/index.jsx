19d79c70d976efa295ce429e2f7037602cd6863e
Gabriel de Campos <gabodecampos@gmail.com>
2023-01-17 03:16:30 -0400

    adjusts on graph component

---
+++ src/components/graph/index.jsx
@@ -0,0 +1,130 @@
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title, PointElement, LineElement
+} from 'chart.js';
+import {
+  Bar,
+  Doughnut, Line
+} from 'react-chartjs-2';
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+);
+
+export const dataPie = {
+  labels: ['Red', 'Blue', 'Yellow'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export const optionsBar = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const dataBarChart = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export const optionsLine = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+  },
+};
+
+export const dataLine = {
+  labels,
+  datasets: [
+    {
+      label: 'Vendas',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Prospecção',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export function GraphDoughnut() {
+  return  <Doughnut data={dataPie} />
+}
+
+export function GraphBarChat(){
+  return <Bar
+    options={optionsBar}
+    data={dataBarChart}
+    style={{
+      width:'100%',
+      height: '15rem'
+    }}
+  />
+}
+
+export function GraphLine() {
+  return <Line
+    options={optionsLine}
+    data={dataLine}
+  />
+}
