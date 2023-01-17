914651de70ff8365f1cd9e5cc328c7c40822792b
Gabriel de Campos <gabodecampos@gmail.com>
2023-01-17 03:17:06 -0400

    working adjusts on dashboard component

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,38 +1,63 @@
 import {
-  Button,
   Card,
   Col,
-  Container,
-  Form,
-  Row
+  Container, Row
+
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+
+import { GraphDoughnut, GraphLine,} from "../graph";
+import DinamicTable from "../dinamicTable";
+import DinamicList from "../list";
 
 function Dashboard() {
   return (
     <Container className="">
       <Row>
         <Col>
-          <Card className="" style={{ width:'20rem'}}>
+          <Card className=""  style={{ width:'16rem'}}>
             <Card.Header>
               Principais produtos vendidos
             </Card.Header>
             <Card.Body>
-              <span>grafico aqui</span>
+              <GraphDoughnut />
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card className="" style={{ width:'20rem'}}>
             <Card.Header>
-              Movimentações
+              Últimas movimentações
             </Card.Header>
             <Card.Body>
-              <span>Lista aqui</span>
+              <DinamicList />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className=""  style={{ width:'30rem'}}>
+            <Card.Header>
+              Fluxo de vendas
+            </Card.Header>
+            <Card.Body>
+              <GraphLine/>
             </Card.Body>
           </Card>
         </Col>
       </Row>
+      <br />
+      <Row className="pb-5">
+        <Col>
+          <Card className="">
+            <Card.Header>
+              Tickets em aberto
+            </Card.Header>
+            <Card.Body>
+              <DinamicTable />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <br />
     </Container>
   );
 }
