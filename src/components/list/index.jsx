41705b414cd624680ed85bc2de0f9d35d7cd8be4
Gabriel de Campos <gabodecampos@gmail.com>
2023-01-17 03:16:17 -0400

    adjusts on list component

---
+++ src/components/list/index.jsx
@@ -0,0 +1,93 @@
+import {
+  ListGroup,
+} from "react-bootstrap";
+
+const movimentation = [
+  {
+    type: 'recebido',
+    description: 'Pgto de Clientes',
+    money: '5000,00'
+  },
+  {
+    type: 'pago',
+    description: 'Hospedagem',
+    money: '700,00'
+  },
+  {
+    type: 'agendado',
+    description: 'Aluguel',
+    money: '1000,00'
+  },
+  {
+    type: 'pago',
+    description: 'Luz',
+    money: '380,00'
+  }
+]
+
+const operationType = {
+  recebido: {
+    icon: 'mdi mdi-arrow-down-bold-hexagon-outline',
+    color: 'green'
+  },
+  pago: {
+    icon: 'mdi mdi-arrow-up-bold-hexagon-outline',
+    color: 'red'
+  },
+  agendado: {
+    icon: 'mdi mdi-arrow-right-bold-hexagon-outline',
+    color: 'darkcyan'
+  },
+}
+
+function DinamicList() {
+  return (
+    <ListGroup variant="flush">
+      <ListGroup.Item
+        className="d-flex justify-content-between align-items-start"
+      >
+        <div>
+          <i
+            className="mdi mdi-arrow-down-bold-hexagon-outline"
+            aria-hidden="true"
+            style={{fontSize:18+'px', color: "green"}}
+          />{' '}
+          <span>recebido</span>
+        </div>
+        <div>
+          <i
+            className="mdi mdi-arrow-up-bold-hexagon-outline"
+            aria-hidden="true"
+            style={{fontSize:18+'px', color: "red"}}
+          />{' '}
+          <span>pago</span>
+        </div>
+        <div>
+          <i
+            className="mdi mdi-arrow-right-bold-hexagon-outline"
+            aria-hidden="true"
+            style={{fontSize:18+'px', color: "darkcyan"}}
+          />{' '}
+          <span>agendado</span>
+        </div>
+      </ListGroup.Item>
+      {movimentation.map((operation) => (
+        <ListGroup.Item
+          className="d-flex justify-content-between align-items-start"
+        >
+          <i
+            className={operationType[operation.type].icon}
+            aria-hidden="true"
+            style={{fontSize:18+'px', color: operationType[operation.type].color}}
+          />
+          <span className="ms-2 me-auto">
+            {operation.description}
+          </span>
+          <span>R$ {operation.money}</span>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+}
+
+export default DinamicList;
