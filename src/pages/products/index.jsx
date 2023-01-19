9a064467fb27a9547da90c7288348f5ffd26bc1e
Gabriel de Campos <gabodecampos@gmail.com>
2023-01-19 11:05:32 -0400

    use props of components

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -2,19 +2,62 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 import Navbar from "../../components/navbar";
-import Dashboard from "../../components/dashboard";
 import Footer from "../../components/footer";
+import DinamicTable from "../../components/dinamicTable/index.jsx";
+import {Container} from "react-bootstrap";
 
 export default function ProductsPage() {
-  console.log('productsPage, user =>', !sessionStorage.getItem('USER'))
-
   if (!sessionStorage.getItem('USER')) return (
     <Navigate to="/" replace={true} />
   )
+  const tableHeader = ['ID', 'NOME', 'VALOR', 'GRUPO', 'AÇÃO']
+  const tableBody = [
+    {
+      id: 1,
+      name: "Caixa de papel",
+      valueMoney: "5000,00",
+      group: "Demanda Interna",
+    },
+    {
+      id: 2,
+      name: "Caixa de papel",
+      valueMoney: "5000,00",
+      group: "Demanda Interna",
+    },
+    {
+      id: 3,
+      name: "Caixa de papel",
+      valueMoney: "5000,00",
+      group: "Demanda Interna",
+    },
+    {
+      id: 4,
+      name: "Caixa de papel",
+      valueMoney: "5000,00",
+      group: "Demanda Interna",
+    },
+    {
+      id: 5,
+      name: "Caixa de papel",
+      valueMoney: "5000,00",
+      group: "Demanda Interna",
+    },
+    {
+      id: 6,
+      name: "Caixa de papel",
+      valueMoney: "5000,00",
+      group: "Demanda Interna",
+    },
+  ]
   return (
     <>
       <Navbar/>
-      <Dashboard/>
+      <Container className="pb-5">
+        <DinamicTable
+          header={tableHeader}
+          body={tableBody}
+        />
+      </Container>
       <Footer/>
     </>
   );
