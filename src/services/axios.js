3df24289a4ad500958b33c0cf6d7f3f8f599e409
Gabriel de Campos <gabodecampos@gmail.com>
2023-01-19 17:48:31 -0400

    change url base of api

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // AXIOS Settings
-const URL_API = 'http://localhost:3000'
+// const URL_API = 'http://localhost:3000'
+const URL_API = 'https://fake-api.herokuapp.com/'
 const TOKEN = null
 
 const http = axios.create({
